18670f9cc67bc6e1bd692cd2cf19b47f54dcd6a2
martinjoo <martin@martinjoo.dev>
2026-08-26 21:25:16 +0200

    Add skill aliases seed

--- api/transform/models/intermediate/int_skill_candidates.sql
+++ api/transform/models/intermediate/int_skill_candidates.sql
@@ -1,14 +1,14 @@
 with
     nofluffjobs_skills as (
         select distinct lower(btrim(skill)) as name,
-        skill as display_name,
+        skill as canonical_name,
         regexp_replace(lower(btrim(skill, ' ')), '\W', '-', 1, 0, 'i') as key
         from {{ ref('int_nofluffjobs__job_skills') }}
     ),
 
     justjoinit_skills as (
         select distinct lower(btrim(skill)) as name,
-        skill as display_name,
+        skill as canonical_name,
         regexp_replace(lower(btrim(skill, ' ')), '\W', '-', 1, 0, 'i') as key
         from {{ ref('int_justjoinit__job_skills') }}
     )
