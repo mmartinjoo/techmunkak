6120b298fb554696b209eed746e414631dce74b0
martinjoo <martin@martinjoo.dev>
2026-08-26 20:40:35 +0200

    Fix issues

--- api/transform/models/intermediate/int_skill_candidates.sql
+++ api/transform/models/intermediate/int_skill_candidates.sql
@@ -1,17 +1,19 @@
 with
     nofluffjobs_skills as (
-        select distinct skill as name,
+        select distinct lower(btrim(skill)) as name,
+        skill as display_name,
         regexp_replace(lower(btrim(skill, ' ')), '\W', '-', 1, 0, 'i') as key
         from {{ ref('int_nofluffjobs__job_skills') }}
     ),
 
     justjoinit_skills as (
-        select distinct skill as name,
+        select distinct lower(btrim(skill)) as name,
+        skill as display_name,
         regexp_replace(lower(btrim(skill, ' ')), '\W', '-', 1, 0, 'i') as key
         from {{ ref('int_justjoinit__job_skills') }}
     )
 
-select name, key from nofluffjobs_skills
+select * from nofluffjobs_skills
 union
-select name, key from justjoinit_skills
+select * from justjoinit_skills
 order by key
