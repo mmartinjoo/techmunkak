af1fcf277cf5d28621cc9941ac6f37f69974fd16
martinjoo <martin@martinjoo.dev>
2026-08-26 19:22:21 +0200

    Convert array daily tasks to string

--- api/transform/models/staging/nofluffjobs/stg_nofluffjobs__jobs.sql
+++ api/transform/models/staging/nofluffjobs/stg_nofluffjobs__jobs.sql
@@ -1,9 +1,12 @@
 select 
   url as url,
   payload->>'title' as title,
-  (payload #>> '{specs, dailyTasks}')::jsonb as daily_tasks,
+  array_to_string(
+    array(select jsonb_array_elements_text(payload['specs']['dailyTasks'])),
+    '; '
+  ) as daily_tasks,
   payload #>> '{basics, category}' as category,
-  payload #>> '{basics, seniority}' as seniority,
+  payload #>> '{basics, seniority, 0}' as seniority,
   payload #>> '{basics, technology}' as technology,
   to_timestamp(payload['posted']::bigint/1000)::timestamptz as posted_at,
   nullif(payload #>> '{company, url}', '') as company_url,
