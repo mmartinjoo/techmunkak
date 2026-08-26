73951fb593e31e3fae899d0e1a3dc71a745d9d60
martinjoo <martin@martinjoo.dev>
2026-08-26 20:34:35 +0200

    Rename columns

--- api/transform/models/intermediate/int_skill_candidates.sql
+++ api/transform/models/intermediate/int_skill_candidates.sql
@@ -1,17 +1,17 @@
 with
     nofluffjobs_skills as (
-        select distinct lower(btrim(skill, ' ')) as skill,
+        select distinct skill as name,
         regexp_replace(lower(btrim(skill, ' ')), '\W', '-', 1, 0, 'i') as key
         from {{ ref('int_nofluffjobs__job_skills') }}
     ),
 
     justjoinit_skills as (
-        select distinct lower(btrim(skill, ' ')) as skill,
+        select distinct skill as name,
         regexp_replace(lower(btrim(skill, ' ')), '\W', '-', 1, 0, 'i') as key
         from {{ ref('int_justjoinit__job_skills') }}
     )
 
-select skill, key from nofluffjobs_skills
+select name, key from nofluffjobs_skills
 union
-select skill, key from justjoinit_skills
-order by skill
+select name, key from justjoinit_skills
+order by key
