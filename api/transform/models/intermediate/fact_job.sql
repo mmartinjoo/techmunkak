cfe718d2f2ec2aea5e01671b5f4fbed900948a50
martinjoo <martin@martinjoo.dev>
2026-08-27 22:44:47 +0200

    Refactor fact job

--- api/transform/models/intermediate/fact_job.sql
+++ api/transform/models/intermediate/fact_job.sql
@@ -1,72 +1,35 @@
 {{ config(materialized='table') }}
 
-with
-	nofluffjobs_jobs as (
-		select
-			j.url,
-			j.title,
-			concat_ws(' ', j.daily_tasks, j.description, j.requirements) as description,
-			s.bottom as monthly_salary_bottom,
-			s.top as monthly_salary_top,
-			s.currency as currency,
-            j.company_name as company_name,
-			j.seniority as seniority,
-			s.contract_type as contract_type,
-			r.region as country_code
-		from {{ ref('stg_nofluffjobs__jobs') }} as j
-		left join {{ ref('int_nofluffjobs__job_salaries') }} as s on j.url = s.url
-		left join {{ ref('int_nofluffjobs__job_regions') }} r on r.url = j.url
-	),
-	
-	justjoinit_jobs as (
-		select
-			j.url,
-			j.title,
-			j.body as description,
-			s.bottom as monthly_salary_bottom,
-			s.top as monthly_salary_top,
-			s.currency as currency,
-            j.company_name as company_name,
-			j.experience_level as seniority,
-			s.contract_type as contract_type,
-			j.country_code as country_code
-		from {{ ref('stg_justjoinit__jobs') }} as j
-		left join {{ ref('int_justjoinit__job_salaries') }} as s on j.url = s.url
-	),
-
-    jobs as (
-        select * from nofluffjobs_jobs 
-        union all 
-        select * from justjoinit_jobs
-    )
-	
 select
-    j.url,
-    j.title,
-    j.description,
+    jobs.job_key,
+	jobs.title,
+	jobs.description,
+	jobs.posted_at,
+	jobs.expires_at,
+	jobs.url,
     c.company_key as company_key,
     s.seniority_key as seniority_key,
     ct.contract_type_key as contract_type_key,
 	country.country_key as country_key,
 	(case
-		when conversions.value > 1.0 then j.monthly_salary_bottom / conversions.value
-		when conversions.value < 1.0 then j.monthly_salary_bottom * conversions.value
-		when conversions.value = 1.0 then j.monthly_salary_bottom
-		else 							  j.monthly_salary_bottom
+		when conversions.value > 1.0 then jobs.monthly_salary_bottom / conversions.value
+		when conversions.value < 1.0 then jobs.monthly_salary_bottom * conversions.value
+		when conversions.value = 1.0 then jobs.monthly_salary_bottom
+		else 							  jobs.monthly_salary_bottom
 	end)::int as monthly_salary_bottom,
 	(case
-		when conversions.value > 1.0 then j.monthly_salary_top / conversions.value
-		when conversions.value < 1.0 then j.monthly_salary_top * conversions.value
-		when conversions.value = 1.0 then j.monthly_salary_top
-		else 							  j.monthly_salary_top
+		when conversions.value > 1.0 then jobs.monthly_salary_top / conversions.value
+		when conversions.value < 1.0 then jobs.monthly_salary_top * conversions.value
+		when conversions.value = 1.0 then jobs.monthly_salary_top
+		else 							  jobs.monthly_salary_top
 	end)::int as monthly_salary_top,
-	coalesce(conversions.to_currency_code, j.currency) as currency_code
-from jobs as j
-left join {{ ref('dim_company') }} as c on c.name = j.company_name
-left join {{ ref('seniority_aliases') }} as sa on sa.raw_value = lower(j.seniority)
+	coalesce(conversions.to_currency_code, jobs.currency_code) as currency_code
+from {{ ref('int_jobs') }} as jobs
+left join {{ ref('dim_company') }} as c on c.name = jobs.company_name
+left join {{ ref('seniority_aliases') }} as sa on sa.raw_value = lower(jobs.seniority)
 left join {{ ref('dim_seniority') }} as s on s.name = sa.canonical_name
-left join {{ ref('contract_type_aliases') }} as cta on cta.raw_value = lower(j.contract_type)
+left join {{ ref('contract_type_aliases') }} as cta on cta.raw_value = lower(jobs.contract_type)
 left join {{ ref('dim_contract_type') }} as ct on ct.name = cta.canonical_name
-left join {{ ref('country_aliases') }} as cnta on cnta.code = lower(j.country_code)
+left join {{ ref('country_aliases') }} as cnta on cnta.code = lower(jobs.country_code)
 left join {{ ref('dim_country') }} as country on country.name = cnta.name
-left join {{ source('bronze', 'currency_conversions') }} as conversions on conversions.from_currency_code = j.currency
+left join {{ source('bronze', 'currency_conversions') }} as conversions on conversions.from_currency_code = jobs.currency_code
