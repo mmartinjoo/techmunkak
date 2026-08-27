f01c7396864d92b45bb8db6ab62e80c362c46ee8
martinjoo <martin@martinjoo.dev>
2026-08-27 22:35:28 +0200

    Refactor countries

--- api/transform/models/intermediate/dim_country.sql
+++ api/transform/models/intermediate/dim_country.sql
@@ -1,28 +1,15 @@
 {{ config(materialized='table') }}
 
 with 
-	nofluffjobs_countries as (
+	countries as (
 		select
-			{{ dim_key('ca.code', 'j.region') }} as country_key,
-			coalesce(ca.name, j.region) as name
-		from {{ ref('int_nofluffjobs__job_regions') }} j
-		left join {{ ref('country_aliases') }} ca
-		on ca.code = lower(j.region)	
-	),
-	
-	justjoinit_countries as (
-		select
-			{{ dim_key('ca.code', 'j.country_code') }} as country_key,
-			coalesce(ca.name, j.country_code) as name
-		from {{ ref('stg_justjoinit__jobs') }} j
-		left join {{ ref('country_aliases') }} ca
-		on ca.code = lower(j.country_code)	
+			{{ dim_key('aliases.code', 'jobs.country_code') }} as country_key,
+			coalesce(aliases.name, jobs.country_code) as name
+		from {{ ref('int_jobs') }} jobs
+		left join {{ ref('country_aliases') }} aliases
+		on aliases.code = lower(jobs.country_code)	
 	)
 
 select country_key, min(name) as name
-from (
-	select * from nofluffjobs_countries
-	union all
-	select * from justjoinit_countries
-)
+from countries
 group by country_key
