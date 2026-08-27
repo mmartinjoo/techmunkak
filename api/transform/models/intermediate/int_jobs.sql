bafa74e2c0e14a536115885de9821e92fc46f2f4
martinjoo <martin@martinjoo.dev>
2026-08-27 21:31:11 +0200

    Add int jobs table

---
+++ api/transform/models/intermediate/int_jobs.sql
@@ -0,0 +1,131 @@
+with 
+	nofluffjobs_skills as (
+		select
+			external_id,
+			jsonb_agg(required_skill) as required_skills,
+			jsonb_agg(nice_to_have_skill) as optional_skills
+		from
+			(select
+				external_id,
+				jsonb_array_elements(must_have_skills) #>> '{value}' as required_skill,
+				jsonb_array_elements(nice_to_have_skills) #>> '{value}' as nice_to_have_skill
+			from {{ ref('stg_nofluffjobs__jobs') }})
+		group by external_id
+	),
+	
+	nofluffjobs_salaries as (
+		select
+		    external_id,        
+		    case
+		        when lower(coalesce(salary #>> '{types,b2b,period}', salary #>> '{types,permanent,period}')) = 'hour' then coalesce(salary #>> '{types,b2b,range,0}', salary #>> '{types,permanent,range,0}')::numeric * 168
+		        when lower(coalesce(salary #>> '{types,b2b,period}', salary #>> '{types,permanent,period}')) = 'day' then coalesce(salary #>> '{types,b2b,range,0}', salary #>> '{types,permanent,range,0}')::numeric * 21
+		        else coalesce(salary #>> '{types,b2b,range,0}', salary #>> '{types,permanent,range,0}')::numeric
+		    end as bottom,
+		    case
+		        when lower(coalesce(salary #>> '{types,b2b,period}', salary #>> '{types,permanent,period}')) = 'hour' then coalesce(salary #>> '{types,b2b,range,1}', salary #>> '{types,permanent,range,1}')::numeric * 168
+		        when lower(coalesce(salary #>> '{types,b2b,period}', salary #>> '{types,permanent,period}')) = 'day' then coalesce(salary #>> '{types,b2b,range,1}', salary #>> '{types,permanent,range,1}')::numeric * 21
+		        else coalesce(salary #>> '{types,b2b,range,1}', salary #>> '{types,permanent,range,1}')::numeric
+		    end as top,
+		    'month' as period,
+		    upper((salary #>> '{currency}')::text) as currency_code,
+		    case
+		        when salary #>> '{types,b2b}' is not null then  'b2b'
+		        else                                            'permanent'
+		    end as contract_type
+		from {{ ref('stg_nofluffjobs__jobs') }}
+	),
+	
+	justjointit_employement_types as (
+		select
+			external_id,
+			jsonb_array_elements(employement_types) as employment_type
+		from {{ ref('stg_justjoinit__jobs') }}
+	),
+	
+	justjointit_employement_types_eur as (
+		select
+			*,
+			row_number() over (
+				partition by external_id
+			) as version
+		from justjointit_employement_types
+		where employment_type #>> '{currency}' = 'EUR'	
+	),
+	
+	justjoinit_salaries as (
+		select 
+			external_id,
+			(employment_type #>> '{from}')::numeric as bottom,
+			(employment_type #>> '{to}')::numeric as top,
+			'month' as period,
+			'EUR' as currency_code,
+			employment_type #>> '{type}' as contract_type
+		from justjointit_employement_types_eur
+		where version = 1
+	),
+	
+	justjoinit_skills as (
+		select
+			external_id,
+			jsonb_agg(required_skill) as required_skills,
+			jsonb_agg(nice_to_have_skill) as optional_skills
+		from
+			(select
+				external_id,
+				jsonb_array_elements(required_skills) #>> '{name}' as required_skill,
+				jsonb_array_elements(nice_to_have_skills) #>> '{name}' as nice_to_have_skill
+			from {{ ref('stg_justjoinit__jobs') }})
+		group by external_id
+	),
+	
+	nofluffjobs_jobs as (
+		select
+			jobs.external_id,
+			jobs.title,
+			concat_ws(' ', jobs.daily_tasks, jobs.description, jobs.requirements) as description,
+			jobs.company_name as company_name,
+			jobs.company_size as company_size,
+			jobs.seniority as seniority,
+			jsonb_array_elements(regions)->>0 as country_code,
+			jobs.posted_at as posted_at,
+			jobs.expires_at as expires_at,			
+			skills.required_skills,
+			skills.optional_skills,
+			salaries.bottom as monthly_salary_bottom,
+			salaries.top as monthly_salary_top,
+			salaries.period as salary_period,
+			salaries.currency_code as currency_code,
+			salaries.contract_type as contract_type,
+			jobs.url
+		from {{ ref('stg_nofluffjobs__jobs') }} as jobs
+		left join nofluffjobs_skills as skills on skills.external_id = jobs.external_id
+		left join nofluffjobs_salaries as salaries on salaries.external_id = jobs.external_id	
+	),
+	
+	justjoinit_jobs as (
+		select
+			jobs.external_id,
+			jobs.title,
+			jobs.body as description,
+			jobs.company_name as company_name,
+			jobs.company_size as company_size,
+			jobs.experience_level as seniority,
+			jobs.country_code as country_code,
+			jobs.posted_at as posted_at,
+			jobs.expires_at as expires_at,
+			skills.required_skills,
+			skills.optional_skills,
+			salaries.bottom as monthly_salary_bottom,
+			salaries.top as monthly_salary_top,
+			salaries.period as salary_period,
+			salaries.currency_code as currency_code,
+			salaries.contract_type as contract_type,
+			jobs.url
+		from {{ ref('stg_justjoinit__jobs') }} as jobs
+		left join justjoinit_skills as skills on skills.external_id = jobs.external_id
+		left join justjoinit_salaries as salaries on salaries.external_id = jobs.external_id	
+	)
+	
+select * from justjoinit_jobs
+union all
+select * from nofluffjobs_jobs
