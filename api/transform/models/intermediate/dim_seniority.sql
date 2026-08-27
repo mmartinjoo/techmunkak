16b0b0d693d9992d9a28e1c348b2bdf56e56e74f
martinjoo <martin@martinjoo.dev>
2026-08-27 22:38:12 +0200

    Refactor seniority

--- api/transform/models/intermediate/dim_seniority.sql
+++ api/transform/models/intermediate/dim_seniority.sql
@@ -1,28 +1,17 @@
 {{ config(materialized='table') }}
 
 with 
-	nofluffjobs_seniorities as (
+	seniorities as (
 		select
-			{{ dim_key('sa.canonical_name', 'j.seniority') }} as seniority_key,
-			coalesce(sa.canonical_name, j.seniority) as name
-		from {{ ref('stg_nofluffjobs__jobs') }} j
-		left join {{ ref('seniority_aliases') }} sa
-		on sa.raw_value = lower(j.seniority)	
-	),
-	
-	justjoinit_seniorities as (
-		select
-			{{ dim_key('sa.canonical_name', 'j.experience_level') }} as seniority_key,
-			coalesce(sa.canonical_name, j.experience_level) as name
-		from {{ ref('stg_justjoinit__jobs') }} j
-		left join {{ ref('seniority_aliases') }} sa
-		on sa.raw_value = lower(j.experience_level)	
+			{{ dim_key('aliases.canonical_name', 'jobs.seniority') }} as seniority_key,
+			coalesce(aliases.canonical_name, jobs.seniority) as name
+		from {{ ref('int_jobs') }} jobs
+		left join {{ ref('seniority_aliases') }} aliases
+		on aliases.raw_value = lower(jobs.seniority)	
 	)
 
-select seniority_key, min(name) as name
-from (
-	select seniority_key, name from nofluffjobs_seniorities
-	union all
-	select seniority_key, name from justjoinit_seniorities
-)
+select 
+	seniority_key, 
+	min(name) as name
+from seniorities
 group by seniority_key
