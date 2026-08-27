7cbbba3e7d290799ea47ca2cd2ccb7846b67ade9
martinjoo <martin@martinjoo.dev>
2026-08-27 22:35:33 +0200

    Refactor contract tpyes

--- api/transform/models/intermediate/dim_contract_type.sql
+++ api/transform/models/intermediate/dim_contract_type.sql
@@ -1,28 +1,17 @@
 {{ config(materialized='table') }}
 
 with 
-	nofluffjobs_contract_types as (
+	contract_types as (
 		select
-			{{ dim_key('cta.canonical_name', 's.contract_type') }} as contract_type_key,
-			coalesce(cta.canonical_name, s.contract_type) as name
-		from {{ ref('int_nofluffjobs__job_salaries') }} s
-		left join {{ ref('contract_type_aliases') }} cta
-		on cta.raw_value = lower(s.contract_type)	
-	),
-	
-	justjoinit_contract_types as (
-		select
-			{{ dim_key('cta.canonical_name', 's.contract_type') }} as contract_type_key,
-			coalesce(cta.canonical_name, s.contract_type) as name
-		from {{ ref('int_justjoinit__job_salaries') }} s
-		left join {{ ref('contract_type_aliases') }} cta
-		on cta.raw_value = lower(s.contract_type)	
+			{{ dim_key('aliases.canonical_name', 'jobs.contract_type') }} as contract_type_key,
+			coalesce(aliases.canonical_name, jobs.contract_type) as name
+		from {{ ref('int_jobs') }} jobs
+		left join {{ ref('contract_type_aliases') }} aliases
+		on aliases.raw_value = lower(jobs.contract_type)	
 	)
 
-select contract_type_key, min(name) as name
-from (
-	select contract_type_key, name from nofluffjobs_contract_types
-	union all
-	select contract_type_key, name from justjoinit_contract_types
-)
+select 
+	contract_type_key, 
+	min(name) as name
+from contract_types
 group by contract_type_key
